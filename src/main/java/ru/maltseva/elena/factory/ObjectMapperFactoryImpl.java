60c3d2fa17fa491314d67c546d769ccb608f63f8
MaltsevaElena <Maltseva.EM@mail.ru>
2022-05-20 20:11:37 +0700

    ObjectMapperFactory

---
+++ src/main/java/ru/maltseva/elena/factory/ObjectMapperFactoryImpl.java
@@ -0,0 +1,17 @@
+package com.epam.izh.rd.online.factory;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ObjectMapperFactoryImpl implements ObjectMapperFactory {
+    private static ObjectMapper objectMapper = new ObjectMapper();
+
+    public ObjectMapperFactoryImpl() {
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
+
+    @Override
+    public ObjectMapper getObjectMapper() {
+        return objectMapper;
+    }
+}
