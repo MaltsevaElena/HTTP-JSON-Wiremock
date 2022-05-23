0552c5c568c659288f96438a421b7a574bc740d2
MaltsevaElena <Maltseva.EM@mail.ru>
2022-05-23 22:22:11 +0700

    creatingPokemonFromJson

--- src/main/java/ru/maltseva/elena/Http.java
+++ src/main/java/ru/maltseva/elena/Http.java
@@ -7,7 +7,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.io.IOUtils;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.*;
@@ -19,12 +18,46 @@ import java.net.URL;
 
 public class Http {
     public static void main(String[] args) throws IOException {
-        String str = "https://pokeapi.co/api/v2/pokemon/squirtle";
-        String ditto = httpRequest(str);
+        String urlSquirtle = "https://pokeapi.co/api/v2/pokemon/squirtle";
+        String squirtleJson = httpRequest(urlSquirtle);
 
+        Pokemon squirtle = creatingPokemonFromJson(squirtleJson);
+        System.out.println(squirtle);
+
+        String urlDiglett = "https://pokeapi.co/api/v2/pokemon/diglett";
+        String diglettJson = httpRequest(urlDiglett);
+
+        Pokemon diglett = creatingPokemonFromJson(diglettJson);
+        System.out.println(diglett);
 
     }
 
+    public static Pokemon creatingPokemonFromJson(String json) throws JsonProcessingException {
+        ObjectMapperFactory mapperFactory = new ObjectMapperFactoryImpl();
+        ObjectMapper mapper = mapperFactory.getObjectMapper();
+
+        Pokemon pokemon = mapper.readValue(json, Pokemon.class);
+
+
+        JsonNode jsonNode = mapper.readValue(json, JsonNode.class);
+        JsonNode stats = jsonNode.get("stats");
+        for (JsonNode jnode : stats) {
+            JsonNode stat = jnode.findValue("stat");
+
+            String name = stat.get("name").asText();
+
+            JsonNode number = jnode.get("base_stat");
+            if (name.equalsIgnoreCase("hp")) {
+                pokemon.setHp(number.shortValue());
+            } else if (name.equalsIgnoreCase("attack")) {
+                pokemon.setAttack(number.shortValue());
+            } else if (name.equalsIgnoreCase("defense")) {
+                pokemon.setDefense(number.shortValue());
+            }
+
+        }
+        return pokemon;
+    }
     public static String httpRequest(String stringUrl) {
         URL url;
         HttpURLConnection connection = null;
@@ -33,8 +66,6 @@ public class Http {
 
         InputStream in;
         String result = " ";
-
-        JSONObject json = null;
 
         try {
             url = new URL(stringUrl);
@@ -52,13 +83,12 @@ public class Http {
 
                 in = new BufferedInputStream(connection.getInputStream());
                 result = IOUtils.toString(in);
-                json = new JSONObject(result);
-                // System.out.println(json);
+
                 in.close();
             } else {
                 System.out.println("GET request not worked");
             }
-        } catch (MalformedURLException | JSONException | ProtocolException e) {
+        } catch (MalformedURLException | ProtocolException e) {
             e.printStackTrace();
         } catch (IOException ioException) {
             ioException.printStackTrace();
