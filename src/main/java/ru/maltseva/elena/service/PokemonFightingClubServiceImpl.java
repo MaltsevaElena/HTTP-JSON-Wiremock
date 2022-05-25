a29764bdde1a5d851e259e63a035a42a4278b166
MaltsevaElena <Maltseva.EM@mail.ru>
2022-05-25 13:25:06 +0700

    Fighting

--- src/main/java/ru/maltseva/elena/service/PokemonFightingClubServiceImpl.java
+++ src/main/java/ru/maltseva/elena/service/PokemonFightingClubServiceImpl.java
@@ -8,11 +8,19 @@ public class PokemonFightingClubServiceImpl implements PokemonFightingClubServic
     private ShowImage showImage;
     @Override
     public Pokemon doBattle(Pokemon p1, Pokemon p2) {
-
+        System.out.println("История боя: ");
         while (p2.getHp() >= 1 && p1.getHp()>=1){
-            doDamage(p1, p2);
-            if (p2.getHp() > 0) {
+            //Первый нападает покемон с наибольшим ID
+            if (p1.getPokemonId()> p2.getPokemonId()) {
+                doDamage(p1, p2);
+                if (p2.getHp() > 0) {
+                    doDamage(p2, p1);
+                }
+            }else {
                 doDamage(p2, p1);
+                if (p1.getHp() > 0) {
+                    doDamage(p1, p2);
+                }
             }
         }
 
@@ -31,6 +39,10 @@ public class PokemonFightingClubServiceImpl implements PokemonFightingClubServic
 
     @Override
     public void doDamage(Pokemon from, Pokemon to) {
-        to.setHp((short) (to.getHp() - from.getAttack()));
+        short damage =  (short) (from.getAttack()-(from.getAttack()* to.getDefense()/100));
+        to.setHp((short) (to.getHp() - damage));
+        System.out.println("Нападает покемон "+from.getPokemonName()+ " c атакой "+ from.getAttack());
+        System.out.println("Покемон "+to.getPokemonName() +" имеет защиту "+to.getDefense()+ "%" );
+        System.out.println("Hp покемона "+to.getPokemonName() + ": "+to.getHp());
     }
 }
